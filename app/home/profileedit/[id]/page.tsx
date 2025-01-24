7388b950fc1e48ce1f515d8bf64912c2e5b09065
tbd-shafa <shafa@technobd.com>
2025-01-24 18:08:46 +0600

    frist commit

--- app/home/profileedit/[id]/page.tsx
+++ app/home/profileedit/[id]/page.tsx
@@ -241,7 +241,7 @@ export default function EditPatientProfile({ params }: { params: { id: string } 
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
+              required/>
             </div>
 
             {/* Gender Selection */}
