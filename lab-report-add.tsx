8277397a6ccb1634096e52bb0568eca8d49eae1c
tbd-shafa <167499847+tbd-shafa@users.noreply.github.com>
2025-03-03 22:03:43 +0600

    Update lab-report-add.tsx

--- lab-report-add.tsx
+++ lab-report-add.tsx
@@ -188,6 +188,7 @@ const LabReportAdd: FC<LabReportAddProps> = ({ patientId }) => {
     setSelectedLabReportAddDcotors((prev) =>
       prev.filter((doc) => doc.id !== doctorId)
     );
+    localStorage.removeItem("selectedLabReportAddDcotors");
   };
 
   useEffect(() => {
